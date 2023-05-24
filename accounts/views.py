c0a233c9626d8d531c4f8d33b430dd70c9a6508b
sangdevil <dnfzja1213@kaist.ac.kr>
2023-05-24 18:16:36 +0900

    login, gallery 80%

--- accounts/views.py
+++ accounts/views.py
@@ -11,14 +11,15 @@ def login(request) :
     if request.method=='POST' :
         
         # data는 forms.form 두번쨰 인자이므로 data = 은 생략 가능
-        print(request)
+        # print(request)
         print(request.POST)
         form = AuthenticationForm(request, data = request.POST) # 먼저 request 인자를 받아야함
+        print(form)
         if form.is_valid() :
             # print("hey")
             # 세션 CREATE/ form.get_user는 User 객체 반환
             auth_login(request, form.get_user())
-            return redirect('articles:index') # 로그인 성공시 메인페이지 이동
+            return redirect('gallery') # 로그인 성공시 메인페이지 이동
     else :
         form = AuthenticationForm()
 
